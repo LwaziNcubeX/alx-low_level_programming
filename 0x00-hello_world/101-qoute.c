3e5b02d4943d02ffc9234958992e05c11c17f788
Lwazi Ncube <111203820+LwaziNcubeX@users.noreply.github.com>
2023-02-17 00:02:40 +0200

    Update 101-qoute.c

--- 0x00-hello_world/101-qoute.c
+++ 0x00-hello_world/101-qoute.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <string.h>
-/** 
+/**
  * main - Prints without printf and puts
- * 
- * Return: Always 0. 
+ *
+ * Return: 1 (success).
  */
 
 int main(void)
 {
-	write(1, "and that piece of art is useful" - Dora Korpar, 2015-10-19", strlen("and that piece of art is useful" - Dora Korpar, 2015-10-19"));
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", strlen);
 
 	return (1);
 }
