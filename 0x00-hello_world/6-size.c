da33fbec7c90134cccf0d9e97930dd03b1e943b9
Lwazi Ncube <lwazincubex@gmail.com>
2023-02-16 12:59:13 -0800

    test01

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *\
+ int main(void)
+ {
+	printf("Size of char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of long int: %zu byte(s)\n", sizeof(LongintType));
+	printf("Size of long long int: %zu byte(s)\n", sizeof(doubleType));
+	printf("Size of float: %zu byte(s)\n", sizeof(floatType));
+
+	return (0);
+}
