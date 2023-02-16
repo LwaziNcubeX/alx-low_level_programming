efcf3520bd15ba1bb8f4ca58953428f3668a9122
Lwazi Ncube <111203820+LwaziNcubeX@users.noreply.github.com>
2023-02-16 23:55:17 +0200

    Print #2

--- 0x00-hello_world/101-qoute.c
+++ 0x00-hello_world/101-qoute.c
@@ -1,1 +1,14 @@
 #include <stdio.h>
+#include <string.h>
+/** 
+ * main - Prints without printf and puts
+ * 
+ * Return: Always 0. 
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful" - Dora Korpar, 2015-10-19", strlen("and that piece of art is useful" - Dora Korpar, 2015-10-19"));
+
+	return (1);
+}
