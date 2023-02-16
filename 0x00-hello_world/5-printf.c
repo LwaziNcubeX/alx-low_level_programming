7ba2cecb643c587a309d3dfcbcfdf068af34aada
Lwazi Ncube <lwazincubex@gmail.com>
2023-02-16 10:40:17 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,8 +5,7 @@
  * Return: always 0 unless stated otherwise...
  */
 int main(void)
-{	char str_3[] = "with proper grammar, but the outcome is a piece of art,";
-
-	printf("%c", *str_3);
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
