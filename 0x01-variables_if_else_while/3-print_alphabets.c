26e33a8ceb7682011ec88005277d181b770d867e
Lwazi Ncube <lwazincubex@gmail.com>
2023-02-17 07:40:28 -0800

    Lp

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return - 0 meaning success
+ */
+int main(void)
+{
+	char Letter;
+	for (Letter = 'a'; Letter <= 'z'; Letter++)
+		putchar(Letter);
+	for (Letter = 'A'; Letter <= 'Z'; Letter++)
+		putchar(letter)
+
+	putchar('\n');
+	return (0);
+}
