4558bc4b72bba1c914633cee11f5e93160d2a1b4
Lwazi Ncube <lwazincubex@gmail.com>
2023-02-22 04:48:06 -0800

    absolutE

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,1 +1,24 @@
-#include
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _abs - entry point
+ *
+ * @n: getting value
+ * Return: end point
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		int abs_val;
+
+		abs_val = n * (-1);
+		return (abs_val);
+	}
+	else
+	{
+		return (n);
+	}
+}
