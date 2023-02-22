de11e4d467bc28862167c53eb8a68fd7e6e6c260
Lwazi Ncube <lwazincubex@gmail.com>
2023-02-22 12:13:24 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - starting point
+ *
+ * @n1: get value
+ * @n2: get value
+ *
+ * Return: results ( success)
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
