3f4fa1191560a16150580da795109ea30cf29c93
Lwazi Ncube <lwazincubex@gmail.com>
2023-02-23 02:12:45 -0800

    test2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,9 +16,17 @@ int largest_number(int a, int b, int c)
     {
         largest = a;
     }
+    else if (a > b && b > c)
+    {
+        largest = a;
+    }
     else if (b > a && a > c)
     {
-        largest = b;
+	    largest = b;
+    }
+    else if (b > c && c > a)
+    {
+	    largest = b;
     }
     else
     {
