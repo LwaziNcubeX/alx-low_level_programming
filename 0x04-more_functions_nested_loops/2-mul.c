310316084ce09c7888178d20174ef632fd1dfd65
Lwazi Ncube <lwazincubex@gmail.com>
2023-02-23 18:19:42 -0800

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - entry point + multiplication os 2 values
+ * @a: assigned numer
+ * @b: assigned number
+ * Return: prodof (success)
+ * prodof - meaning product of
+ */
+int mul(int a, int b)
+{
+	int prodof = 0;
+
+	prodof = a * b;
+	return (prodof);
+}
