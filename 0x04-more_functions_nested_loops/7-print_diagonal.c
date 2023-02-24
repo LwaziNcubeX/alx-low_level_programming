6250a085ede6e9b699defcc98f69e0a45b2187a5
Lwazi Ncube <lwazincubex@gmail.com>
2023-02-24 04:56:26 -0800

    parallel universe

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Entry Point
+ * @n: assigned value
+ */
+
+
+void print_diagonal(int n)
+{
+	int a;
+	int b;
+
+	if (n <= 0)
+		_putchar('\n');
+	for (a = 0; a < n; a++)
+	{
+		for (b = 0; b < a; b++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+
+}
