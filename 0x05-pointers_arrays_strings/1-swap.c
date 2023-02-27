3277033b0f6013f4468033bec39568d7d7f220ae
Lwazi Ncube <lwazincubex@gmail.com>
2023-02-27 04:28:27 -0800

    wap or swap ?

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * reset_to_98 - Entry Point
+ * @a: pointer of an int
+ * @b: pointer of an int
+ * Return: Void
+ */
+
+void swap_int(int *a, int *b)
+{
+
+	*a = 42;
+	*b = 98;
+
+}
