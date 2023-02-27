0a5132204e05b89bd9d247f343ee704627664043
Lwazi Ncube <lwazincubex@gmail.com>
2023-02-27 04:46:17 -0800

    int swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * reset_to_98 - Entry Point
+ * swap_int - Entry Point
  * @a: pointer of an int
  * @b: pointer of an int
  * Return: Void
@@ -8,7 +8,8 @@
 
 void swap_int(int *a, int *b)
 {
-
+	*a = 98;
+	*b = 42;
 	*a = 42;
 	*b = 98;
 
