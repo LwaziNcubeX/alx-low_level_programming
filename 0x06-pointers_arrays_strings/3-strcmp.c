22d1df41420118bb3f93a840177034494af9b731
Lwazi Ncube <lwazincubex@gmail.com>
2023-03-02 04:18:53 -0800

    a function that compares two strings.

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,24 +8,19 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
+	int s1val;
+	int s2val;
 
+/* Iterate through both strings until we find a different character*/
 	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
 	{
 		i++;
 	}
 
-	if (s1[i] == s2[i])
-	{
-		return (0);
-	}
+/* Assign values to the characters in both strings*/
+	s1val = s1[i] == '\0' ? 0 : s1[i];
+	s2val = s2[i] == '\0' ? 0 : s2[i];
 
-	else if (s1[i] > s2[i])
-	{
-		return (1);
-	}
-
-	else
-	{
-		return (-1);
-	}
+/* Return the difference between the values of the characters*/
+	return (s1val - s2val);
 }
