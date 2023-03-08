9b3064f3d17dcbad7067d6a5a22899a60fba22c0
Lwazi Ncube <lwazincubex@gmail.com>
2023-03-08 06:46:15 -0800

    Locked away

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts_recursion - Entry Point
+ * @s: assigned char
+ * Return: string
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(s);
+	_putchar('\n');
+
+	return (0);
+}
