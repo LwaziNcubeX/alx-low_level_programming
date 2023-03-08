0a647e8254b9cd4b5119d3bd36f9a34890ef4731
Lwazi Ncube <lwazincubex@gmail.com>
2023-03-08 14:37:00 -0800

    a function that returns the factorial of a given number.

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * factorial - Entry Point
+ * @s: assigned char
+ * Return: something
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return  (1);
+	else
+		return (n * factorial (n - 1));
+}
