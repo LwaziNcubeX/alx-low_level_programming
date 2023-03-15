6f3746560b01244f15e645cebe4b4487b4125923
Lwazi Ncube <lwazincubex@gmail.com>
2023-03-14 18:02:18 -0700

    wildcard.

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,17 +1,19 @@
 #include "main.h"
+
 /**
  * wildcmp - compares two strings
- * @s1: the first string
- * @s2: the second string (may contain wildcard *)
- * Return: 1 if strings are identical, otherwise 0
+ * @s1: first string
+ * @s2: second string (may contain wildcards)
+ *
+ * Return: 1 if the strings can be considered identical, otherwise 0.
  */
-int wildcmp(char *s1, char s2)
+int wildcmp(char *s1, char *s2)
 {
-	if (s1 == '\0' && s2 == '\0')
+	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-	if (s2 == '')
-		return ((wildcmp(s1, s2 + 1)) || (wildcmp(s1 + 1, s2)))
-	if (s1 == *s2)
+	if (*s2 == '*')
+		return ((wildcmp(s1, s2 + 1)) || (wildcmp(s1 + 1, s2)));
+	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 	return (0);
 }
