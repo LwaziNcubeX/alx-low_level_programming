d904f05b23f651739182b490793feb8fe0153b53
Lwazi Ncube <lwazincubex@gmail.com>
2023-03-16 18:04:41 -0700

    test

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "stdio.h"
 #include <stdlib.h>
 /**
   * malloc_checked - allocates memory using malloc
@@ -7,10 +8,14 @@
   */
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
+	void *ptr = malloc(b);
 
-	ptr = malloc(b);
+
 	if (ptr == NULL)
+	{
+		printf("Error: malloc failed.. :)\n");
 		exit(98);
+
+	}
 	return (ptr);
 }
