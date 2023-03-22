eb78e500e340133dbefc1f56a92b7f49c4220ba0
Lwazi Ncube <lwazincubex@gmail.com>
2023-03-22 16:44:24 -0700

    rm.uneccesary code

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,16 +2,11 @@
 /**
  * print_name - prints a name
  * @name: pointer to a string containing the name to be printed
- * @f: pointer to a function that will be used to print each character of the name
+ * @f: pointer to a function
  *
  * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-	int i;
-
-	for (i = 0; name[i] != '\0'; i++)
-	{
-		f(&name[i]);
-	}
+		(*f)(name);
 }
