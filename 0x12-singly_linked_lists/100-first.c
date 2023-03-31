4f33e6d80b1d7f3900d8d30dc521f9287f20180a
Lwazi Ncube <lwazincubex@gmail.com>
2023-03-30 18:02:28 -0700

    Afunction that prints a string before main function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,28 @@
 #include "lists.h"
 #include <stdio.h>
 
-void print_first()
+/**
+ * print_first - entry point
+ *
+ * Return: void
+ */
+void print_first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	char a[] = "You're beat! and yet, you must allow,";
+
+	printf("%s\nI bore my house upon my back!\n", a);
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char b[] = "A tortoise, having pretty good sense of";
+
+	printf("(%s a hare's nature, challenges one to a race.)\n", b);
+
+	return (0);
 }
