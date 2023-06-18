4dc1553f82cdf6b75a61995ab68d9e854ebe61a8
LwaziNcubeX <lwazincubex@gmail.com>
2023-06-18 17:06:52 +0200

    A function that prints an integer.

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,11 +2,28 @@
 #include <stdio.h>
 
 /**
- * print_number - entry point
+ * print_number - prints an integer.
  *
  * @n: assigned value
  */
 
 void print_number(int n)
 {
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+
+
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+	_putchar('0' + n % 10);
 }
