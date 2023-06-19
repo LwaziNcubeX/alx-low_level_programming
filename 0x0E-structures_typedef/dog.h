80d83e2894beda70772987495aa4fa386a572b9e
LwaziNcubeX <lwazincubex@gmail.com>
2023-06-20 01:06:58 +0200

    A guarded header containg prototypes of all tasks

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,6 +14,10 @@ typedef struct dog
 	char *owner;
 } dog;
 
-int main(void);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */
