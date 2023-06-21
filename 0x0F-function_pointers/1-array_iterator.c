1fc42153aa3ae3c9c865f5be032438d577c1a3b3
LwaziNcubeX <lwazincubex@gmail.com>
2023-06-22 01:48:30 +0200

    FIX 1-array_iterator.c:4: WARNING: line over 80 characters (90)

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - executes a function given as a parameter on each element of an array.
+ * array_iterator - executes a function given as a parameter
  * @size: size of array
  * @array: array
  * @action: pointer to a function
