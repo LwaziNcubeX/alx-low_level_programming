c696c0106cf6b469242c61c096f09fe611c0f174
LwaziNcubeX <lwazincubex@gmail.com>
2023-07-01 00:58:37 +0200

    A function that prints a function output before the main function output

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * print_first - entry point
+ *
+ * Return: void
+ */
+void print_first(void)
+{
+    printf("You're beat! and yet, you must allow,\n");
+    printf("I bore my house upon my back!\n");
+}
