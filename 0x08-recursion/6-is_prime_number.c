0f7e4f6e2453f809a35a57305bf5780876ebc127
LwaziNcubeX <lwazincubex@gmail.com>
2023-07-05 02:39:34 +0200

    Removed any kind of Loops since they are not allowed for this task

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,18 +10,23 @@ int is_prime_number(int n)
 	int i;
 
 	if (n < 2)
-	{
 		return (0);
-	}
 
-	i = 2;
-	while (i <= n / 2)
+	if (n == 2)
+		return (1);
+
+	if ((n % 2) == 0)
+		return (0);
+
+	i = 3;
+
+	if (i * i <= n)
 	{
 		if (n % i == 0)
 		{
 			return (0);
 		}
-		i += 1;
+		i += 2;
 	}
 
 	return (1);
