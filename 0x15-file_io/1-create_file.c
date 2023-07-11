fc8f0c35357fc01d7de8fb30f1115d93d65e3948
LwaziNcubeX <lwazincubex@gmail.com>
2023-07-11 03:27:11 +0200

    Added an if statement text_content != NULL

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,15 +25,15 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	if (text_content == NULL)
-		return (-1);
-
-	sz = write(fd, text_content, strlen(text_content));
-	if (sz == '\0')
+	if (text_content != NULL)
 	{
-		close(fd);
-		free(buffer);
-		return (-1);
+		sz = write(fd, text_content, strlen(text_content));
+		if (sz == '\0')
+		{
+			close(fd);
+			free(buffer);
+			return (-1);
+		}
 	}
 
 	close(fd);
