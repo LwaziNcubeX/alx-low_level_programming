9f3131f00d54dff1f70f123a9583fa5d12a29c54
LwaziNcubeX <lwazincubex@gmail.com>
2023-07-11 02:27:52 +0200

    A function that creates a file.

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * create_file - creates a new file
+ * @filename: name of the file
+ * @text_content: text to write
+ *
+ * Return: 1 on success or -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, sz;
+	char *buffer;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_RDWR | O_CREAT, 600);
+	if (fd == -1)
+		return (-1);
+
+	buffer = (char *) malloc(sizeof(char) * (*text_content + 1));
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	sz = write(fd, text_content, strlen(text_content));
+	if (sz == '\0')
+	{
+		close(fd);
+		free(buffer);
+		return (-1);
+	}
+
+	close(fd);
+	free(buffer);
+
+	return (1);
+}
