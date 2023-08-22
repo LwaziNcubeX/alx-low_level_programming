79441cd65d834b96ac951222866272e4bb6522e1
LwaziNcubeX <lwazincubex@gmail.com>
2023-08-22 02:14:19 +0200

    change /root to /tmp to avoid using sudo

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /root https://github.com/LwaziNcubeX/alx-low_level_programming/blob/main/0x18-dynamic_libraries/sixcalls.so
-export LD_PRELOAD=/root/sixcalls.so
+wget -P /tmp https://github.com/LwaziNcubeX/alx-low_level_programming/blob/main/0x18-dynamic_libraries/sixcalls.so
+export LD_PRELOAD=/tmp/sixcalls.so
